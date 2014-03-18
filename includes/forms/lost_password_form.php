789b8a71720623a096149d6147f788d90356453b
Behtash Fani <iranmen2006@gmail.com>
2014-03-18 18:51:49 +0430

    lostpassword temp 1

--- includes/forms/lost_password_form.php
+++ includes/forms/lost_password_form.php
@@ -1,6 +1,11 @@
+<div class="lost_password">
 <form name="form1" method="post" target="_self" action="<?php echo $setting['site_url'];?>/index.php?task=lost_password&step=2">
- <?php echo LP_EMAIL;?><br />
+ <h3>
+ <?php echo LP_EMAIL;?>
+ </h3>
+ <br />
     <input name="email" id="email" class="form_textbox"><br /><br />
     
       <input type="submit" name="Submit" value="<?php echo LP_BUTTON1;?>" class="dropdown-submit">
 </form>
+</div>
